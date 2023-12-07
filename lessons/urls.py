eb674d3433de54a7b8cd81737e2dc3568e224dc5
dariacasey <caseydaria14@yahoo.com>
2023-12-06 23:11:48 -0600

    Added charts

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import LessonDetail, AllLessons
 from . import views
 
@@ -7,9 +7,7 @@ urlpatterns = [
     path('lesson/<int:pk>/', LessonDetail.as_view(), name='lesson_detail'),
     path('lesson/<int:pk>/questions', views.view_lesson_exercises, name='lesson_exercises'),
     path('progress/', views.grades_view, name='progress_page'),
-    # path('lesson/teacher-grades/', views.teacher_progress, name='teacher_progress'),
     path('', AllLessons.as_view(), name="all_lessons"),
     path('lesson/<int:pk>/grade/', views.grade_exercises, name='grade_exercises'),
-    # path('lesson/create/', views.create_lesson, name='create_lesson'),
 
 ]
